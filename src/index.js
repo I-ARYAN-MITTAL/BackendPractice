c378bcde114cda90a8d00cdf73c5453a618bd0f3
I-ARYAN-MITTAL <aryanmittal7942@gmail.com>
2024-03-27 00:16:11 +0530

    utils created

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ dotenv.config({
 })
 
 connectDB()
+.then(process.env.PORT || 8000 , () =>{
+    console.log(`server is running at port${process.env.PORT}`)
+})
+.catch((err) =>{
+    console.log("MongoDB connection failed",err);
+})
